655ab7c4fe40fa3805532dac59adb10b0fd6bd25
Duncan Murchison <duncancmurchison@gmail.com>
2022-05-19 12:20:03 -0400

    merge_sort

---
+++ sorting_algos.rb
@@ -0,0 +1,25 @@
+
+# -- Merge Sort --
+def merge_sort(array)
+  return array if array.length <= 1
+
+  mid = array.length / 2
+  left = array.take(mid)
+  right = array.drop(mid)
+  left_sorted, right_sorted = merge_sort(left), merge_sort(right)
+  merge(left_sorted, right_sorted)
+end
+
+def merge(left, right)
+  merged = []
+  until left.empty? || right.empty?
+    if left.first < right.first
+      merged.push(left.shift)
+    else
+      merged.push(right.shift)
+    end
+  end
+  merged + left + right
+end
+
+p merge_sort([5,3,2,5,7,4,2,1,4,2])
